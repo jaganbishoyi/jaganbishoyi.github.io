a90e83656fe889c8225d43e9696366b5592456c0
Jagan Mohan Bishoyi <jaganmohanbishoyi@gmail.com>
2024-08-16 09:46:49 +0530

    isPlatformBrowser Vs isPlatformServer in angular and CSS enhancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, Injector, OnInit, PLATFORM_ID } from '@angular/core'
 import { ISeoEssentials } from '@interfaces/general.interface';
 import { UtilsService } from '@services/utils.services';
 import { REQUEST_URL } from './request-url.service';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -24,40 +24,6 @@ export class AppComponent implements OnInit {
             const reqUrl = this.injector.get(REQUEST_URL);
 
             switch (reqUrl) {
-                // case "/testimonials": {
-                //     this.addCanonicalTag("https://URL/testimonials");
-                //     const SEOData: IMetaInfo = {
-                //         title: "Testimonial |Blog",
-                //         site_name: "blog site |Blog",
-                //         description: "Check out what our job seekers and employers think of us.",
-                //         url: "https://URL/testimonials",
-                //         image: "assets/Images/home-page.jpg",
-                //         twitterTitle: "Testimonial | blog",
-                //     };
-                //     this.updateMetaTag(SEOData);
-                //     this.addTestimonialsToBody();
-                //     break;
-                // }
-                // case "/districts": {
-                //     this.http.get<number>(
-                //         `${this.constantService.getHostingURL()}/GetTotalDistrictCount?token=${token}`,
-                //         options
-                //     ).subscribe((count: number) => {
-                //         this.addCanonicalTag("https://URL/districts");
-                //         const SEOData: IMetaInfo = {
-                //             title: "url offers opportunities in " + count + " districts spanning New York, New Jersey, Connecticut, Massachusetts, Pennsylvania, Vermont, and the neighboring regions. |Blog",
-                //             site_name: "blog site |Blog",
-                //             description: "url offers opportunities in " + count + " districts spanning New York, New Jersey, Connecticut, Massachusetts, Pennsylvania, Vermont, and the neighboring regions.",
-                //             url: "https://URL/districts",
-                //             image: "assets/Images/home-page.jpg",
-                //             twitterTitle: "blog site |Blog",
-                //         };
-
-                //         this.updateMetaTag(SEOData);
-                //     });
-                //     break;
-                // }
-
                 case "/": {
                     const SEOData: ISeoEssentials = {
                         title: "Jagan Mohan Bishoyi | Angular | Frontend Web Developer",
@@ -70,59 +36,29 @@ export class AppComponent implements OnInit {
                     break;
                 }
 
-                default: {
-                    // if (reqUrl.includes("/job-details/")) {
-                    //     const jobNumber = reqUrl.split('/')[reqUrl.split('/').length - 1];
+                // case "/projects": {
+                //     const SEOData: ISeoEssentials = {
+                //         title: "Projects",
+                //         description: "Projects",
+                //         canonicalLink: 'https://jaganb.dev/projects',
+                //         image: 'https://jaganb.dev/assets/images/me.png'
+                //     };
 
-                    //     this.http.get<IJobMetaInfo>(
-                    //         `${this.constantService.getHostingURL()}/GetJobDetails?JobNumber=${jobNumber}&token=${token}`,
-                    //         options
-                    //     ).subscribe((data: IJobMetaInfo) => {
-                    //         this.addCanonicalTag("https://URL/job-details/" + data.JobNumber);
-                    //         const SEOData: IMetaInfo = {
-                    //             title: data.JobDescription + " opening in " + data.DistrictName,
-                    //             site_name: "blog site |Blog",
-                    //             description: "Apply for " + data.JobDescription + " opening in " + data.DistrictName,
-                    //             url: "https://URL/jobs",
-                    //             image: "assets/Images/home-page.jpg",
-                    //             twitterTitle: "blog site |Blog",
-                    //         };
-                    //         this.addJobDetailsToBody(data);
-                    //         if (data && !data.IsInstructionalJob) {
-                    //             this.addGoogleJobDetailsToBody(data);
-                    //         }
-                    //         this.updateMetaTag(SEOData);
-                    //     });
-                    // } else {
+                //     this.utils.setSEOEssentials(SEOData);
+                //     break;
+                // }
 
-                    //     if (reqUrl && !reqUrl.includes('.') && !reqUrl.includes('?') && !reqUrl.includes('undefined') && !reqUrl.includes('timeout')) {
-                    //         const label = reqUrl.split("/");
+                // case "/blogs": {
+                //     const SEOData: ISeoEssentials = {
+                //         title: "Blogs",
+                //         description: "Blogs",
+                //         canonicalLink: 'https://jaganb.dev/blogs',
+                //         image: 'https://jaganb.dev/assets/images/me.png'
+                //     };
 
-                    //         if (label.length === 2 && label[1].length) {
-                    //             const districtLabel = label[1];
-                    //             this.http.get<IDistrictMetaInfo>(
-                    //                 `${this.constantService.getHostingURL()}/GetDistrictInformation?districtLabel=${districtLabel}&token=${token}`,
-                    //                 options
-                    //             ).subscribe((data: IDistrictMetaInfo) => {
-                    //                 this.addCanonicalTag("https://URL/" + districtLabel);
-                    //                 const SEOData: IMetaInfo = {
-                    //                     title: data.result.DistrictName + ' |Blog',
-                    //                     site_name: "blog site |Blog",
-                    //                     description: `Learn about working at ${data.result.DistrictName} in ${data.result.City}, ${data.result.State}, ${data.result.RegionName}. See jobs, salaries,and more for ${data.result.DistrictName}, ${data.result.City} location.`,
-                    //                     url: "https://URL/" + districtLabel,
-                    //                     image: "assets/Images/home-page.jpg",
-                    //                     twitterTitle: data.result.DistrictName + ' |Blog',
-                    //                 };
-
-                    //                 this.addDistrictDetailsToBody(data);
-                    //                 this.updateMetaTag(SEOData);
-                    //             });
-                    //         }
-                    //     }
-                    // }
-
-                    break;
-                }
+                //     this.utils.setSEOEssentials(SEOData);
+                //     break;
+                // }
             }
         }
     }
